f011e68ef1c2c3ff8c8521552ddf3958c9726b64
João Lucas <lucas.26.11.2014@gmail.com>
2022-12-27 09:55:40 -0300

    Atualização Scroll
    Atualização Scroll

--- src/features/global.js
+++ src/features/global.js
@@ -1,14 +1,51 @@
+/* eslint-disable no-constant-condition */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-useless-escape */
 /* eslint-disable no-undef */
+/* eslint-disable no-dupe-class-members */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import { Dragdealer } from 'dragdealer'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 export function globalCode() {
   var btn = $('.scroll-to-top')
+  var btn2 = $('.menu_link')
 
   btn.on('click', function (e) {
     e.preventDefault()
     $('html, body').animate({ scrollTop: 0 }, '300')
   })
-}
 
+  $('.menu_link.p-profissionais').on('click', function (e) {
+    $('.tab-contabilidade.profissionais').click()
+    return false
+  })
+
+  $('.menu_link.p-clinicas').on('click', function (e) {
+    $('.tab-contabilidade.clinicas').click()
+    return false
+  })
+
+  $('.tab-contabilidade.clinicas').on('click', function (e) {
+    $('html,body').animate({
+      scrollTop: $('.div-trick').offset().top,
+    })
+    $('.menu_link.p-clinicas').addClass('w--current')
+    $('.menu_link.p-profissionais').removeClass('w--current')
+
+    return false
+  })
+
+  $('.tab-contabilidade.profissionais').on('click', function (e) {
+    $('html,body').animate({
+      scrollTop: $('.div-trick').offset().top,
+    })
+    $('.menu_link.p-profissionais').addClass('w--current')
+    $('.menu_link.p-clinicas').removeClass('w--current')
+
+    return false
+  })
+}
 // eslint-disable-next-line no-unused-vars

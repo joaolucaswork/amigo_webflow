99eeb17ce88cdf31cb2c96e809b071725a2818ca
João Lucas <lucas.26.11.2014@gmail.com>
2023-01-03 10:43:06 -0300

    Editar
    Editar

--- src/features/amigo_contabil/amigoContabil.js
+++ src/features/amigo_contabil/amigoContabil.js
@@ -1,29 +1,24 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 import gsap from 'gsap'
-import stickybits from 'stickybits'
 
 import { extraNavbarFunction } from './features_amigo-contabil/extraNavbarFunction'
 import { formFunction } from './features_amigo-contabil/formFunction'
+import { strickTrickFunction } from './features_amigo-contabil/stickTrickFunction'
 import { tabelaFunction } from './features_amigo-contabil/tabelaFunction'
 
 export function amigoContabil() {
   $(function () {
     if ($('body').is('.amigo-contabil')) {
-      $(function () {
-        stickybits('.nossos-planos-profissionais-top_wrapper', {
-          stickyBitStickyOffset: 0,
-        })
-      })
-
       $('.open-faq-button').on('click', function (e) {
-        $('.accordion').click()
+        $('.accordion-clinica-servicos').click()
         return false
       })
 
       extraNavbarFunction()
       formFunction()
       tabelaFunction()
+      strickTrickFunction()
     }
   })
 }
---
+++ src/features/amigo_contabil/features_amigo-contabil/stickTrickFunction.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+import stickybits from 'stickybits'
+
+export function strickTrickFunction() {
+  $(function () {
+    stickybits('.nossos-planos-profissionais-top_wrapper', {
+      stickyBitStickyOffset: 0,
+      useStickyClasses: true,
+    })
+  })
+}
